d3094a9a3ab3593f7c8621ffb8a3b177f1218bf6
Jack Zhou <J.Zhou@televic.com>
2019-05-28 16:28:59 +0800

    first commit

---
+++ CSharp7/OutVariables.cs
@@ -0,0 +1,33 @@
+﻿using Infrastructure;
+using System;
+using static System.Console;
+
+namespace CSharp7
+{
+    public class OutVariables : IDemo
+    {
+        public float Title => 1.1f;
+
+        public string Description => "C# 7 - OutVariables";
+
+        public void Run()
+        {
+            DateTime dt;
+            if (DateTime.TryParse("01/01/2019", out dt))
+            {
+                WriteLine($"Old-fashioned parse: {dt}");
+            }
+
+            if (DateTime.TryParse("02/02/2019", out var dt2))
+            {
+                WriteLine($"New parse: {dt2}");
+            }
+
+            int.TryParse("615", out var i);
+            WriteLine($"i = {i}");
+
+            int.TryParse("abc", out var j);
+            WriteLine($"j = {j}");
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,34 @@
+﻿using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using static System.Console;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var objs = new List<IDemo>();
+            foreach(var file in Directory.GetFiles(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "*.dll"))
+            {
+                foreach (var demo in Assembly.LoadFile(file).DefinedTypes.Where(o => o.ImplementedInterfaces.Contains(typeof(IDemo))))
+                {
+                    objs.Add(Activator.CreateInstance(demo) as IDemo);
+                }
+                objs = new List<IDemo>(objs.OrderBy(o => o.Title));
+            }
+            objs.ForEach(o => WriteLine($"{o.Title} : {o.Description}"));
+            WriteLine("Please enter the number that you want to run ?");
+            float.TryParse(ReadLine(), out var title);
+            foreach (var want in objs.OrderBy(o => o.Title).Where(o => o.Title == title))
+            {
+                want.Run();
+            }
+            Read();
+        }
+    }
+}
