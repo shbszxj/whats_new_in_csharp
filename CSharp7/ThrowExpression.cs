a6c14360240fdc6c094d32219d6fb5ffaf461574
Jack Zhou <J.Zhou@televic.com>
2019-05-29 13:59:06 +0800

    throw expression

---
+++ CSharp7/ThrowExpression.cs
@@ -0,0 +1,48 @@
+﻿using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CSharp7.ThrowExpression
+{
+    public class ThrowExpression : Demo<ThrowExpression>
+    {
+        public override float Title => 1.8f;
+
+        public override void Run()
+        {
+            int v = -1;
+            try
+            {
+                var te = new Expression("");
+                v = te.GetValue(-1); // does not get defaulted!
+            }
+            catch (Exception e)
+            {
+                WriteLine(e);
+            }
+            finally
+            {
+                WriteLine(v);
+            }
+        }
+    }
+
+    public class Expression
+    {
+        public string Name { get; set; }
+
+        public Expression(string name)
+        {
+            Name = name ?? throw new ArgumentNullException(paramName: nameof(Name));
+        }
+
+        public int GetValue(int n)
+        {
+            return n > 0 ? n + 1 : throw new Exception();
+        }
+    }
+}
