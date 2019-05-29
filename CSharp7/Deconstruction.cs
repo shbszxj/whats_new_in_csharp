cc7cacf5f758f44ba3478c15ab56751be57ef5fa
Jack Zhou <J.Zhou@televic.com>
2019-05-29 11:35:41 +0800

    deconstruction

---
+++ CSharp7/Deconstruction.cs
@@ -0,0 +1,45 @@
+﻿using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CSharp7.Deconstruction
+{
+    public class Deconstruction : Demo<Deconstruction>
+    {
+        public override float Title => 1.4f;
+
+        public override void Run()
+        {
+            // deconstruction
+            Point pt = new Point { X = 2, Y = 3 };
+            var (i, j) = pt;
+            WriteLine($"Got a point i = {i}, j = {j}");
+
+            // can also discard values
+            (int z, _) = pt;
+            WriteLine($"z = {z}");
+        }
+    }
+
+    public class Point
+    {
+        public int X, Y;
+
+        // Deconstructions into a single element are not supported in C# 7.0.
+        public void Deconstruct(out string s)
+        {
+            s = $"{X}-{Y}";
+        }
+
+        public void Deconstruct(out int x, out int y)
+        {
+            x = X;
+            y = Y;
+        }
+    }
+
+}
--- CSharp7/Tuples.cs
+++ CSharp7/Tuples.cs
@@ -55,33 +55,6 @@ namespace CSharp7
             // there's no result.sum unless you add it to signature
             // var snp = new Func<double, double, (double, double)>((a, b) => (sum: a + b, product: a * b));
             WriteLine($"sum = {result.sum}, product = {result.product}");
-
-            // deconstruction
-            Point pt = new Point { X = 2, Y = 3 };
-            var (i, j) = pt;
-            WriteLine($"Got a point i = {i}, j = {j}");
-
-            // can also discard values
-            (int z, _) = pt;
-            WriteLine($"z = {z}");
         }
     }
-
-    class Point
-    {
-        public int X, Y;
-
-        // Deconstructions into a single element are not supported in C# 7.0.
-        public void Deconstruct(out string s)
-        {
-            s = $"{X}-{Y}";
-        }
-
-        public void Deconstruct(out int x, out int y)
-        {
-            x = X;
-            y = Y;
-        }
-    }
-
 }
