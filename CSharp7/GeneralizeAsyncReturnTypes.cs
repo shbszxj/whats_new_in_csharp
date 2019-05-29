169f07b0e08dcb318dd0364f0d7ab2d7bd98b66b
Jack Zhou <J.Zhou@televic.com>
2019-05-29 15:05:08 +0800

    value task

---
+++ CSharp7/GeneralizeAsyncReturnTypes.cs
@@ -0,0 +1,41 @@
+﻿using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CSharp7
+{
+    public class GeneralizeAsyncReturnTypes : Demo<GeneralizeAsyncReturnTypes>
+    {
+        public override float Title => 1.9f;
+
+        public override void Run()
+        {
+            WriteLine(GetDirSize2(@"c:\temp").Result);
+        }
+
+        static async Task<long> GetDirSize(string dir)
+        {
+            if (!Directory.EnumerateFileSystemEntries(dir).Any())
+            {
+                return 0;
+            }
+
+            return await Task.Run(() => Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories).Sum(f => new FileInfo(f).Length));
+        }
+
+        static async ValueTask<long> GetDirSize2(string dir)
+        {
+            if (!Directory.EnumerateFileSystemEntries(dir).Any())
+            {
+                return 0;
+            }
+
+            return await Task.Run(() => Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories).Sum(f => new FileInfo(f).Length));
+        }
+    }
+}
