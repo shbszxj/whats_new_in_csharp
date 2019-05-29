93697c3d727b6285f5c63a4e8352f7d1e5406d00
Jack Zhou <J.Zhou@televic.com>
2019-05-29 16:56:49 +0800

    default literal in c # 7.1

---
+++ CSharp7.1/DefaultLiteral.cs
@@ -0,0 +1,45 @@
+﻿using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CSharp7._1
+{
+    public class DefaultLiteral : Demo<DefaultLiteral>
+    {
+        public override int Title => 21;
+
+        public override void Run()
+        {
+            int a = default;
+            WriteLine($"{nameof(a)} = {a}");
+
+            const int c = default;
+            WriteLine($"const {nameof(c)} = {c}");
+
+            // will not work here
+            // const int? d = default; // oops
+
+            // cannot leave defaults on their own
+            var e = new[] { default, 33, default };
+            WriteLine(string.Join(",", e));
+
+            string s = default;
+            WriteLine($"default of string is equals null ? {s == null}");
+
+            // ternary operations
+            var x = a > 0 ? default : 1.5;
+            WriteLine($"type of {nameof(x)} is {x.GetType().Name}");
+
+            WriteLine($"default value of DateTime = {GetTimestamps()}");
+        }
+
+        DateTime GetTimestamps(List<int> items = default)
+        {
+            return default;
+        }
+    }
+}
