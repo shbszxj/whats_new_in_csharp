27dd963438b1426dc1ec783e6470f2bccdca3923
Jack Zhou <J.Zhou@televic.com>
2019-05-30 10:31:05 +0800

    Leading underscores in numeric literals

---
+++ CSharp7.2/LeadingUnderscoresNumericSeparators.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CSharp7._2
+{
+    public class LeadingUnderscoresNumericSeparators : Demo<LeadingUnderscoresNumericSeparators>
+    {
+        public override int Title => 31;
+
+        public override void Run()
+        {
+            // binary
+            var x = 0b_1111_0000;
+
+            // hex 
+            var y = 0x_baad_f00d;
+
+            WriteLine($"binary x = {x}");
+            WriteLine($"hex y = {y}");
+        }
+    }
+}
