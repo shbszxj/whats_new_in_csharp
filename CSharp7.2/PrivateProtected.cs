dc48ff17acc225237bb8560e032d145b4d9f8a36
Jack Zhou <J.Zhou@televic.com>
2019-05-30 10:45:16 +0800

    private protected

---
+++ CSharp7.2/PrivateProtected.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure;
+
+namespace CSharp7._2
+{
+    public class PrivateProtected : Demo<PrivateProtected>
+    {
+        public override int Title => 32;
+
+        public override void Run()
+        {
+            Base pp = new Base();
+            var d = new Derived();
+            d.b = 3;
+            // d.c is a no-go
+        }
+
+        class Base
+        {
+            private int a;
+            // derived classes or classes in same assembly
+            protected internal int b;
+            // containing class or derived classes in same assembly only
+            private protected int c;
+        }
+
+        class Derived : Base
+        {
+            public Derived()
+            {
+                c = 333;
+                b = 3;
+            }
+        }
+    }
+}
