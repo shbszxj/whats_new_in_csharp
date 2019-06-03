d3ed26bd1de33e70ff72c4212e2222fe55431a86
Zhou, Xujun <xu-jun.zhou@outlook.com>
2019-06-03 20:35:02 +0800

    non trailing named arguments

---
+++ CSharp7.2/NonTrailingNamedArguments.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CSharp7._2
+{
+    public class NonTrailingNamedArguments : Demo<NonTrailingNamedArguments>
+    {
+        public override int Title => 33;
+
+        public override void Run()
+        {
+            DoSomething(33, bar: 44);
+
+            DoSomething(foo: 33, 44);
+
+            DoSomething(bar: 33, foo: 44);
+        }
+
+        void DoSomething(int foo, int bar)
+        {
+            WriteLine($"{nameof(foo)}: {foo}, {nameof(bar)}: {bar}");
+        }
+    }
+}
