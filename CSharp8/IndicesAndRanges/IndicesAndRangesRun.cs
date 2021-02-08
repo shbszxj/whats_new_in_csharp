93b636402eb97120ecb284fa10f709e3acd4cf25
JZH <J.Zhou@televic.com>
2021-02-08 15:54:23 +0800

    c# 8: Indices and Ranges

---
+++ CSharp8/IndicesAndRanges/IndicesAndRangesRun.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp8.IndicesAndRanges
+{
+    public class IndicesAndRangesRun
+    {
+        private readonly string[] _words;
+
+        public IndicesAndRangesRun(string[] words)
+        {
+            _words = words;
+        }
+
+        public string[] GetAllWords()
+        {
+            return _words[..];
+        }
+
+        public string GetWord(int index)
+        {
+            return _words[index];
+        }
+
+        public string GetWordsFromStart(int start, int end)
+        {
+            return ParseStringArrayToString(_words[start..end]);
+        }
+
+        public string GetWordsFromEnd(int start, int end)
+        {
+            return ParseStringArrayToString(_words[^start..^end]);
+        }
+
+        private string ParseStringArrayToString(string[] array)
+        {
+            return string.Join(",", array);
+        }
+    }
+}
---
+++ CSharp8/Tests/IndicesAndRangesTests.cs
@@ -0,0 +1,57 @@
+﻿using CSharp8.IndicesAndRanges;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CSharp8.Tests
+{
+    public class IndicesAndRangesTests : BaseTest
+    {
+        private readonly string[] _words = new string[]
+        {
+            "The",
+            "quick",
+            "brown",
+            "fox",
+            "jumped",
+            "over",
+            "the",
+            "lazy",
+            "dog"
+        };
+
+        public IndicesAndRangesTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void GetWordDog()
+        {
+            var runner = new IndicesAndRangesRun(_words);
+            Assert.Equal("dog", runner.GetWord(8));
+        }
+
+        [Fact]
+        public void GetWordsLazyDog()
+        {
+            var runner = new IndicesAndRangesRun(_words);
+            Assert.Equal("lazy,dog", runner.GetWordsFromStart(7, 9));
+        }
+
+        [Fact]
+        public void GetWordsLazyDog2()
+        {
+            var runner = new IndicesAndRangesRun(_words);
+            Assert.Equal("lazy,dog", runner.GetWordsFromEnd(2, 0));
+        }
+
+        [Fact]
+        public void GetAllWords()
+        {
+            var runner = new IndicesAndRangesRun(_words);
+            Assert.Equal(_words, runner.GetAllWords());
+        }
+    }
+}
